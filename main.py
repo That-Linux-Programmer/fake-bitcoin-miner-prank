fafc43743fe922b35d6b1384a73794550ca60bef
Nitro-Live <100413904+That-Linux-Programmer@users.noreply.github.com>
2022-12-09 22:45:11 +0000

    Updating main.py
    Adding a new chance to get address in code

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import time
 print("If you want this to work put it in an IDE instead of this. And then remove the \"break\" command")
 print("Sorry i could not get colorama to work. press enter to continue")
 print()
-input(
-)
+input()
 print()
 bit = 0.00
 while True:
@@ -18,6 +17,10 @@ while True:
         print("0x" + ''.join(random.choice(lowercase) for i in range(40)) ,"(",bit,"B)")
         time.sleep(3)
     elif digit ==  66:
+        bit = bit + 0.01
+        print("0x" + ''.join(random.choice(lowercase) for i in range(40)) ,"(",bit,"B)")
+        time.sleep(3)
+    elif digit == 43:
         bit = bit + 0.01
         print("0x" + ''.join(random.choice(lowercase) for i in range(40)) ,"(",bit,"B)")
         time.sleep(3)
